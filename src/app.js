67d9554f70033c08480e4d98166d43f5206e7ff4
Atticool <129253601+atticuslburn@users.noreply.github.com>
2025-08-28 17:37:33 +1000

    change db to be testdb and maindb. you will need to create 2 files in the home directory "testdb", "maindb"

--- src/app.js
+++ src/app.js
@@ -4,11 +4,18 @@ const sqlite3 = require('sqlite3').verbose();
 let sql;
 
 //connect to db
-const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
+const testdb = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+})
+const maindb = new sqlite3.Database("./main.db", sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message);
 })
 //create table
-sql = 'CREATE TABLE test(id INTEGER PRIMARY KEY AUTOINCREMENT)';
-db.run(sql);
+// maindb.run('DROP TABLE IF EXISTS test');
+// testdb.run('DROP TABLE IF EXISTS test');
+// sql = 'CREATE TABLE test(id INTEGER PRIMARY KEY AUTOINCREMENT)';
+// testdb.run(sql);
+// maindb.run(sql);
 
 //drop table
+// testdb.run('SELECT id FROM test');
