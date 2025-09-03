94941e592b76e79473b0540ce2f37cd716551b60
Lorin-Chanel <87113663+lorinchanel@users.noreply.github.com>
2025-09-03 15:16:41 +1000

    Add SQL Statements for Nutrition Table and Add Database

--- src/sql/app.js
+++ src/sql/app.js
@@ -3,22 +3,9 @@ const require = createRequire(import.meta.url);
 const sqlite3 = require('sqlite3').verbose();
 
 //connect to db
-const testdb = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.error(err.message);
-})
 const maindb = new sqlite3.Database("./main.db", sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message);
 })
-//create table
-// testdb.run('DROP TABLE IF EXISTS test');
-// testdb.run('CREATE TABLE IF NOT EXISTS test(id INTEGER, number INTEGER)');
 
-// testdb.run('INSERT INTO test VALUES (1, 1)');
-// testdb.run('SELECT id, number FROM test');
-
-// let sql;
-// sql = 'INSERT INTO test VALUES (?, ?)'
-// testdb.run(sql, [12345, 67], (err) => {
-//     if (err) return console.error(err.message);
-// })
+export default maindb;
 
--- src/sql/sql.js
+++ src/sql/sql.js
@@ -2,32 +2,59 @@
     Add SQL strings to the array, and it will run them in order
 
 */
+
+import maindb from "./app.js";
+
 const sql_array = [
     //DROP TABLES
     'DROP TABLE IF EXISTS pantry_items',
+    'DROP TABLE IF EXISTS nutrition',
 
     //CREATE TABLES
     `CREATE TABLE pantry_items (
-        pantry_id INTEGER,
+        pantry_id INTEGER PRIMARY KEY,
         item_name VARCHAR(100),
         quantity DECIMAL(20, 5),
         unit VARCHAR(50)
     )`,
 
-
-
-
-
-
-
+    `CREATE TABLE IF NOT EXISTS nutrition(nutrition_id INTEGER PRIMARY KEY, pantry_id INTEGER, calories , protein , carbs , fats , FOREIGN KEY(pantry_id) REFERENCES pantry_items(pantry_id))`,
 
     //INSERT DATA
-    'INSERT INTO pantry_items VALUES (0, "Apple", 4, "units")',
-    'INSERT INTO pantry_items VALUES (0, "Flour", 2.5, "kilograms")',
+    'INSERT INTO pantry_items VALUES (1, "Apple", 4, "units")',
+    'INSERT INTO pantry_items VALUES (2, "Flour", 2.5, "kilograms")',
+    'INSERT INTO pantry_items VALUES (3, "Milk", 1, "litre")',
+    'INSERT INTO pantry_items VALUES (4, "Oats", 500, "grams")',
+    'INSERT INTO pantry_items VALUES (5, "Yogurt", 170, "grams")',
+    'INSERT INTO pantry_items VALUES (6, "Bananas", 5, "kilograms")',
+    'INSERT INTO pantry_items VALUES (7, "Chicken Breast", 500, "grams")',
+    'INSERT INTO pantry_items VALUES (8, "Rib Eye Steak", 200, "grams")',
+
+    'INSERT INTO nutrition(pantry_id, calories, protein, carbs, fats) VALUES (3, 155, 8.5, 11.0, 8.5)',
+    'INSERT INTO nutrition(pantry_id, calories, protein, carbs, fats) VALUES (4, 231, 6.2, 31.8, 1.4)',
+    'INSERT INTO nutrition(pantry_id, calories, protein, carbs, fats) VALUES (5, 143, 12.2, 12.7, 9.9)',
+    'INSERT INTO nutrition(pantry_id, calories, protein, carbs, fats) VALUES (6, 210, 1.4, 19.6, 1)',
+    'INSERT INTO nutrition(pantry_id, calories, protein, carbs, fats) VALUES (7, 189, 31, 0, 3.6)',
+    'INSERT INTO nutrition(pantry_id, calories, protein, carbs, fats) VALUES (8, 414, 24.3, 1, 35.2)',
+
+    'SELECT * FROM nutrition JOIN pantry_items ON nutrition.pantry_id = pantry_items.pantry_id'
+
 ]
 
 //For loop so it runs in order
-for (let i = 0; i < sql_array.length; i++) {
-    let sql = sql_array[i];
-    maindb.run(sql_array[i]);
-}
+maindb.serialize (() => {
+    for (let i = 0; i < sql_array.length; i++) {
+        if(sql_array[i].trim().split(/\s+/)[0] === 'SELECT'){
+            maindb.all(sql_array[i], [], (err, row) => {
+                if(err) return console.error(err.message);
+                row.forEach(row => {
+                    console.log(row);
+                })
+            });
+        } else {
+            let sql = sql_array[i];
+            maindb.run(sql_array[i]);
+        }
+        
+    }
+});
