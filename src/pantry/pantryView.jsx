1d1ee90837a1b3492d0220c2a35bd26e0ff19f46
Atticool <129253601+atticuslburn@users.noreply.github.com>
2025-09-04 16:45:48 +1000

    update pantryview and added light / dark mode pantry card colours

--- src/pantry/pantryView.jsx
+++ src/pantry/pantryView.jsx
@@ -4,12 +4,31 @@ FEATURES:
 - View all items in given pantry
 */
 
-function PantryView() {
+function PantryView(pantry_id) {
     return (
         <div class="section">
-            <h1>View of Pantry []</h1>
-            <div class="genericContentBox">
-                
+            <h1>View of Pantry </h1>
+            <div>
+                <button>Filters</button>
+                <button>Order By</button>
+                <input id="search-bar" placeholder='Search...' type="text"/>
+            </div>
+            <div class="pantryCards">
+                <div class="pantryCard">
+                    <h2>Apple</h2>
+                    <p>Extra Information: Lorem Ipsum Dolor Sit Amet</p>
+                    <p>Amount: 4 Units</p>
+                </div>
+                <div class="pantryCard">
+                    <h2>Flour</h2>
+                    <p>Extra Information: Lorem Ipsum Dolor Sit Amet</p>
+                    <p>Amount: 2.5 Kilograms</p>
+                </div>
+                <div class="pantryCard">
+                    <h2>Milk</h2>
+                    <p>Extra Information: Lorem Ipsum Dolor Sit Amet</p>
+                    <p>Amount: 1 Litre</p>
+                </div>
             </div>
         </div>
     );
