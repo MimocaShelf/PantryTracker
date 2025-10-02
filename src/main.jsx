484f47459a0265674eec54731bed0274ec102584
Atticool <129253601+atticuslburn@users.noreply.github.com>
2025-10-02 16:10:14 +1000

    move main back

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ root.render(<App />);*/
 import './style.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './src/app.jsx';
+import App from './app.jsx';
 
 const root = createRoot(document.getElementById('app'));
 root.render(<App />);
