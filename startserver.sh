7dfd45a399c8e0fed68d51120e5fbbbbceeee2d1
Atticool <129253601+atticuslburn@users.noreply.github.com>
2025-10-26 23:38:02 +1100

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -1,7 +1,11 @@
-npm install
+echo 'Running Server'
+npm install 2>&1 >/tmp/npm_install.out
+echo 'Home Directory'
 pwd >&2
 # ls -lR >&2
 cd server 
+echo 'Starting Backend Server' >&2
 node server.js & 
 cd .. 
-npm run
+echo 'Starting Frontend Server' >&2
+npm run 
